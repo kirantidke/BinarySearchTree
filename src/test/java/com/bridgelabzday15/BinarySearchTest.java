85759c36b0bcb72aa7941aac243d00121ea0c86f
kirantidke <kirantidke2013@gmail.com>
2021-06-27 15:23:20 +0530

    "[kiran] added file"

---
+++ src/test/java/com/bridgelabzday15/BinarySearchTest.java
@@ -0,0 +1,19 @@
+package com.bridgelabzday15;
+import com.bridgelabz.BinaryTree;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+
+public class BinarySearchTest {
+    @Test
+    public void given3Numbers_WhenAddedToBST_ShouldReturnSize3() {
+        BinaryTree<Integer> binaryTree = new BinaryTree<>();
+        binaryTree.add(56);
+        binaryTree.add(30);
+        binaryTree.add(70);
+        int size = binaryTree.getSize();
+        Assert.assertEquals(3, size);
+    }
+}
